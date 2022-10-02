8b3999de4732b4bbfc2bf8d26ddb38f122fdc964
Berke Kocadere <berkekocadere@gmail.com>
2022-10-02 15:40:14 +0300

    TodoList task completed.

---
+++ Hands-on-Tasks/TodoList.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.7; 
+
+contract TodoList {
+    struct Todo {
+        string text; 
+        bool completed; 
+    }
+
+    Todo[] public todos; 
+
+    function create(string calldata _text) external {
+        todos.push(Todo({text: _text, completed: false}));
+    }
+
+    function updateText(uint _index, string calldata _text) external {
+        // Cheaper if we have only one field 
+        todos[_index].text = _text; 
+
+        // Below code will do exactly the same, cheaper if we have multiple fields  
+        // Todo storage todo = todos[_index];  
+        // todo.text = _text
+    }
+
+    function get(uint _index) external view returns (string memory, bool) {
+        // Storage costs less gas 
+        Todo memory todo = todos[_index]; 
+        return (todo.text, todo.completed);  
+    }
+
+    function toggleCompleted(uint _index) external {
+        todos[_index].completed = !todos[_index].completed; 
+    } 
+}   
